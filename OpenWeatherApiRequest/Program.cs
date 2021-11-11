9b2bb09ab0089d35e21bf9194406178010ddfb37
Azat Stepanov <azat.stepanov86@mail.ru>
2021-11-11 16:57:20 +0300

    Init

---
+++ OpenWeatherApiRequest/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using OpenWeatherApiRequest.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenWeatherApiRequest
+{
+    class Program
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        static async Task Main(string[] args)
+        {
+            await StartAsync();
+        }
+
+        public static async Task StartAsync()
+        {
+            WeatherData weatherData = new WeatherData();
+
+            while (true)
+            {
+                Console.Write("\nYour city? ");
+                var city = Console.ReadLine();
+
+                string info = null;
+
+                try
+                {
+                    info = await weatherData.GetInfoAsync(city);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.Write("\nWrong name if city");
+                    _logger.Info($"City: {city}");
+                    _logger.Error(ex.Message);
+                }
+
+                Console.WriteLine($"\n{info}");
+
+                Console.Write("Do you want repeat? (any button - repeat, n - exit) : ");
+                var answer = Console.ReadKey();
+
+                if (answer.Key == ConsoleKey.N)
+                    break;
+            }
+        }
+    }
+}
---
+++ OpenWeatherApiRequest/WeatherData.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using OpenWeatherApiRequest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace OpenWeatherApiRequest
+{
+    public class WeatherData
+    {
+        private readonly string _apiId;
+
+        public WeatherData()
+        {
+            _apiId = ConfigurationManager.AppSettings["apiId"];
+        }
+
+        public async Task<string> GetInfoAsync(string city)
+        {
+            var weatherInfo = await GetWeatherAsync(city);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Weather in {weatherInfo.Name}:");
+            sb.AppendLine($"temperature {weatherInfo.Main.Temp} °С, feel like {weatherInfo.Main.FeelsLike} °C");
+            sb.AppendLine($"{weatherInfo.Weather.FirstOrDefault().Description}");
+            sb.AppendLine($"wind is {weatherInfo.Wind.Speed} m/s");
+
+            return sb.ToString();
+        }
+
+        private async Task<WeatherInfo> GetWeatherAsync(string city)
+        {     
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={_apiId}");
+            
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(content);
+
+            return weatherInfo;
+        }
+    }
+}
